951c789d7a80fcc85f3f76c4ea1df13291a17f8a
rikborgo <162338242+rikborgo@users.noreply.github.com>
2024-05-21 09:46:49 +0200

    Update main.R

--- main.R
+++ main.R
@@ -35,55 +35,39 @@ legend("topright", legend = c("Normal", "Trend"),
 # ISYSMART
 pre.period <- as.Date(c("2023-06-15", "2023-07-01"))
 post.period <- as.Date(c("2023-07-02", "2023-09-09"))
-impact_isysmart <- CausalImpact(df, pre.period, post.period, model.args=list(niter=10000))
-plot(impact_isysmart, c('original'))
+impact_isysmart <- CausalImpact(df, pre.period, post.period)
+plot(impact_isysmart)
 print(summary(impact_isysmart))
 print(summary(impact_isysmart, "report"))
 
 # ISYGIFT
-subset_pre <- subset(data, Data <= "2023-09-09")
-nrow_pre <- nrow(subset_pre)
-subset_post <- subset(data, Data > "2023-09-09" & Data <= "2023-10-22")
-nrow_post <- nrow(subset_post)
-subdata <- subset(data, Data <= "2023-10-22")
-subdata_final <- subdata[,c(2,3)]
-impact_isygift <- CausalImpact(subdata_final, c(1, nrow_pre), c(nrow_pre+1, nrow(subdata_final)))
+pre.period <- as.Date(c("2023-06-15", "2023-09-09"))
+post.period <- as.Date(c("2023-09-10", "2023-10-15"))
+impact_isygift <- CausalImpact(df, pre.period, post.period)
 plot(impact_isygift)
 print(summary(impact_isygift))
 print(summary(impact_isygift, "report"))
 
 # ISYCASHBACK
-subset_pre <- subset(data, Data <= "2023-11-15")
-nrow_pre <- nrow(subset_pre)
-subset_post <- subset(data, Data > "2023-11-15" & Data <= "2024-01-14")
-nrow_post <- nrow(subset_post)
-subdata <- subset(data, Data <= "2024-01-14")
-subdata_final <- subdata[,c(2,3)]
-impact_isycashback <- CausalImpact(subdata_final, c(1, nrow_pre), c(nrow_pre+1, nrow(subdata_final)))
+pre.period <- as.Date(c("2023-06-15", "2023-11-15"))
+post.period <- as.Date(c("2023-11-16", "2024-01-07"))
+impact_isycashback <- CausalImpact(df, pre.period, post.period)
 plot(impact_isycashback)
 print(summary(impact_isycashback))
 print(summary(impact_isycashback, "report"))
 
 # MEMBER GET MEMBER
-subset_pre <- subset(data, Data <= "2023-12-21")
-nrow_pre <- nrow(subset_pre)
-subset_post <- subset(data, Data > "2023-12-21" & Data <= "2024-03-20")
-nrow_post <- nrow(subset_post)
-subdata <- subset(data, Data <= "2024-03-20")
-subdata_final <- subdata[,c(2,3)]
-impact_isymgm <- CausalImpact(subdata_final, c(1, nrow_pre), c(nrow_pre+1, nrow(subdata_final)))
+pre.period <- as.Date(c("2023-06-15", "2023-12-21"))
+post.period <- as.Date(c("2023-12-22", "2024-03-13"))
+impact_isymgm <- CausalImpact(df, pre.period, post.period)
 plot(impact_isymgm)
 print(summary(impact_isymgm))
 print(summary(impact_isymgm, "report"))
 
 # ISYGIFT2
-subset_pre <- subset(data, Data <= "2024-04-29")
-nrow_pre <- nrow(subset_pre)
-subset_post <- subset(data, Data > "2023-04-29" & Data <= "2024-05-15")
-nrow_post <- nrow(subset_post)
-subdata <- subset(data, Data <= "2024-05-15")
-subdata_final <- subdata[,c(2,3)]
-impact_isygift2 <- CausalImpact(subdata_final, c(1, nrow_pre), c(nrow_pre+1, nrow(subdata_final)))
+pre.period <- as.Date(c("2023-06-15", "2024-04-29"))
+post.period <- as.Date(c("2024-04-30", "2024-06-17"))
+impact_isygift2 <- CausalImpact(df, pre.period, post.period)
 plot(impact_isygift2)
 print(summary(impact_isygift2))
 print(summary(impact_isygift2, "report"))
